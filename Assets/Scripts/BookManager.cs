f5ced29dca24abb2dee81d4b35aa0633a054f7bf
Jasmine Roberts <jasminearvr@gmail.com>
2024-05-23 11:43:42 +0500

    commit lfs in main

--- Assets/Scripts/BookManager.cs
+++ Assets/Scripts/BookManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:540c742935ec4fd52826ad207d54d986599b0b02f46cf879935a50ab0c2d03de
-size 10992
+oid sha256:7d0927674ae896e9d8beeb76444c515f0fbf7d9d9a3746b82ddcedb313e802a3
+size 11520
