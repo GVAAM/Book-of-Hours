029be0263f52c0e3c5e63e17791e3ec11e22167d
Jasmine Roberts <jasminearvr@gmail.com>
2024-05-26 23:14:12 +0500

    chnages 26 May

---
+++ Assets/Scripts/ScriptsMine/ListComponents.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00ecacddd248f826cf375958db5b607ed351c3cfa233c651b1cc0133442f36f5
+size 435
---
+++ Assets/Scripts/ScriptsMine/RightPageTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c17a3386827b1926239c1e0b40370e96e44c751c6cc9df8d23cc09a7eb4a043d
+size 1361
